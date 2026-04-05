4de8de6287dc4cac2659f4b43b0e276de4290d44
zerotwo1910 <83332887+zerotwo1910@users.noreply.github.com>
2026-04-05 19:01:26 +0530

    Delete Souce Codes directory

--- Souce Codes/VehicleOrderBatchScheduler.java
+++
@@ -1,6 +0,0 @@
-global class VehicleOrderBatchScheduler implements Schedulable {
-    global void execute(SchedulableContext sc) {
-        VehicleOrderBatch batchJob = new VehicleOrderBatch();
-        Database.executeBatch(batchJob, 50); // 50 = batch size
-    }
-}
